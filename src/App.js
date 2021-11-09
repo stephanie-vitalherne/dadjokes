f5a1b9e941ea8b3900a5a46e9a1af96c8427fde3
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-11-09 10:23:20 -0500

    inital commit - testing api fetch and getting a strange CORS error. waiting to see what to do next

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
 function App() {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://joke.deno.dev/10")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Jokes</h1>
     </div>
   );
 }
